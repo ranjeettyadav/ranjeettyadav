621f399719c99984912e038de246cfef5116dfcd
ranjeettyadav <46112390+ranjeettyadav@users.noreply.github.com>
2020-05-25 19:14:31 +0530

    Update example_gcs_to_bq_operator.py

--- example_gcs_to_bq_operator.py
+++ example_gcs_to_bq_operator.py
@@ -1,3 +1,17 @@
+from airflow.models import Connection
+from airflow.settings import Session
+
+session = Session()
+gcp_conn = Connection(
+    conn_id='bigquery',
+    conn_type='google_cloud_platform',
+    extra='{"extra__google_cloud_platform__project":"dmgcp-ingestion-poc"}')
+if not session.query(Connection).filter(
+        Connection.conn_id == gcp_conn.conn_id).first():
+    session.add(gcp_conn)
+    session.commit()
+
+
 import airflow
 try:
     from airflow.contrib.operators import gcs_to_bq
