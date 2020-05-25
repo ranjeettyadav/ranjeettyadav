4645d9ad3953125a10f055aa59563a97c76e7d27
ranjeettyadav <46112390+ranjeettyadav@users.noreply.github.com>
2020-05-25 17:24:48 +0530

    Create example_gcs_to_bq_operator.py

---
+++ example_gcs_to_bq_operator.py
@@ -0,0 +1,44 @@
+import airflow
+#try:
+#    from airflow.contrib.operators import gcs_to_bq
+#except ImportError:
+#    gcs_to_bq = None
+from airflow import models
+from airflow.operators import bash_operator
+
+
+#if gcs_to_bq is not None:
+#    args = {
+#        'owner': 'airflow',
+#        'start_date': airflow.utils.dates.days_ago(2)
+#    }
+
+    dag = models.DAG(
+        dag_id='example_gcs_to_bq_operator', default_args=args,
+        schedule_interval=None)
+
+#    create_test_dataset = bash_operator.BashOperator(
+#        task_id='create_airflow_test_dataset',
+#        bash_command='bq mk airflow_test',
+#        dag=dag)
+
+    # [START howto_operator_gcs_to_bq]
+    load_csv = gcs_to_bq.GoogleCloudStorageToBigQueryOperator(
+        task_id='gcs_to_bq_example',
+        bucket='kubernetes-staging-85897c950b',
+        source_objects=['airflow_data_file/us-states.csv'],
+        destination_project_dataset_table='transient.gcs_to_bq_table',
+        schema_fields=[
+            {'name': 'name', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'post_abbr', 'type': 'STRING', 'mode': 'NULLABLE'},
+        ],
+        write_disposition='WRITE_TRUNCATE',
+        dag=dag)
+    # [END howto_operator_gcs_to_bq]
+
+#    delete_test_dataset = bash_operator.BashOperator(
+#        task_id='delete_airflow_test_dataset',
+#        bash_command='bq rm -rf airflow_test',
+#        dag=dag)
+
+    load_csv
