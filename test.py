3f69b7617c6cf895dfdacf6b49ab0bd21be506ca
ranjeettyadav <46112390+ranjeettyadav@users.noreply.github.com>
2020-05-27 12:25:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,8 @@ run_this = BashOperator(
     task_id='run_this',
    # use_legacy_sql=False,
     bash_command="bq query --nouse_legacy_sql 'SELECT count(*) FROM `dmgcp-ingestion-poc`.transient.cvn_stress_8gb' ",
-    dag=dag
+    dag=dag,
+    bigquery_conn_id=my_gcp_connection
 )
 run_this
 
