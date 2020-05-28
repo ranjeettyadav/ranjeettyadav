e2c93177e663055f3a3c33a6f54653d4d39e51ae
BH11623 <manjotsingh.chug@macys.com>
2020-05-28 18:59:50 +0530

    code change

---
+++ test_dags/ddl/sample.sql
@@ -0,0 +1,1 @@
+create table `dmgcp-ingestion-poc.airflow_test.sample` as (select *,current_datetime as insert_time from `dmgcp-ingestion-poc.airflow_test.gcs_to_bq_table`)
---
+++ test_dags/sample.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow import models
+from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+import importlib
+import airflow
+
+project_dm = 'dmgcp-ingestion-poc'
+location = 'US'
+bq_connection_id= 'bigquery_default'
+
+default_dag_args = {
+    'owner': 'airflow',
+    'depends_on_past': False, 
+    'start_date': airflow.utils.dates.days_ago(0),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+    'project_id': project_dm
+}
+
+dag = DAG(
+    dag_id='sample',
+    default_args=default_dag_args,
+    schedule_interval='0 0 * * *',
+)
+
+create_sample = BigQueryOperator(
+    task_id='create_sample',
+    sql='sample.sql',
+    bigquery_conn_id=bq_connection_id,
+    use_legacy_sql=False,
+    dag=dag
+    )
+
+create_sample
