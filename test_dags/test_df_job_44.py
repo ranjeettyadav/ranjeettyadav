1dfc5f44142faf90c6fb55d7482fb2e63c474ca9
BH11623 <manjotsingh.chug@macys.com>
2020-06-05 00:09:42 +0530

    code change

--- test_dags/test_df_job_44.py
+++ test_dags/test_df_job_44.py
@@ -19,7 +19,7 @@ default_args = {
 # define the dag
 dag = DAG('test_df_job_44', # give the dag a name
 		   description = 'Test_Job',
-           schedule_interval=None,
+           schedule_interval='*/5 * * * *',
            default_args=default_args
          )
 
