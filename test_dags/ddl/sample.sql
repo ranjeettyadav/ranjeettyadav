f7422de0cf8d2158a405465f052d05681fd11d99
ranjeettyadav <46112390+ranjeettyadav@users.noreply.github.com>
2020-06-04 20:25:12 +0530

    Update sample.sql

--- test_dags/ddl/sample.sql
+++ test_dags/ddl/sample.sql
@@ -1,1 +1,1 @@
-create table `dmgcp-ingestion-poc.airflow_test.sample` as (select *,current_datetime as insert_time from `dmgcp-ingestion-poc.airflow_test.gcs_to_bq_table`)
+create or replace table `dmgcp-ingestion-poc.airflow_test.sample` as (select *,current_datetime as insert_time from `dmgcp-ingestion-poc.airflow_test.gcs_to_bq_table`)
