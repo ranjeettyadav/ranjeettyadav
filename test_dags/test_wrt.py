09ee5298f4a3d1ff6f70754bce76aecaf4c5a0e7
BH11623 <manjotsingh.chug@macys.com>
2020-06-08 14:17:10 +0530

    code change

---
+++ test_dags/test_wrt.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow import models
+from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+import importlib
+import airflow
+
+project_dm = 'dmgcp-ingestion-poc'
+location = 'US'
+bq_connection_id= 'bigquery_default'
+
+default_dag_args = {
+    'owner': 'airflow',
+    'depends_on_past': False, 
+    'start_date': airflow.utils.dates.days_ago(0),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+    'project_id': project_dm
+}
+
+dag = DAG(
+    dag_id='test_wrt',
+    default_args=default_dag_args,
+    schedule_interval=none,
+)
+
+load_csv = gcs_to_bq.GoogleCloudStorageToBigQueryOperator(
+    task_id='gcs_to_bq_example',
+    bucket='ebcidc-to-bq-testing-us-central',
+    source_objects=['csv/wrt/wrt_output.csv'],
+    destination_project_dataset_table='airflow_test.wrt_1',
+    schema_objects='gs://kubernetes-staging-85897c950b/Dags/json/wrt-00000-of-00001.json',
+    write_disposition='WRITE_TRUNCATE',
+    dag=dag,
+    bigquery_conn_id=bq_connection_id,           
+    google_cloud_storage_conn_id=bq_connection_id)
+
+load_csv
+
+
